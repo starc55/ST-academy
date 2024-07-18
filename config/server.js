69f5cf0bd2c9760273aef60098674770512e28f9
Ogabek <orziyevogabek67@gmail.com>
2024-07-18 21:11:29 +0500

    Initial commit

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "93744a3bb473a959debbe6a1e858ecafe444948f363378d2c9a4f6dba38f6848",
+      "75d5800ba102b3699ff43a1c3ef799bda580ce92ca3d9af39558066e6507c9bc",
+    ]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
