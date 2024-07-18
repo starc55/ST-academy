74e88289bae8b9013fbc2eb4e675bb8e9da1c46f
Ogabek <orziyevogabek67@gmail.com>
2024-07-18 20:44:03 +0500

    Initial commit

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','7f9bcab553ab34759b20f3233416a5102e1a39cf53ed21850719b5034292707ac81bd5ce77298c811402f3543129a339ff7d39a5dddabe5f6296a2b1b850cdd1'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
